ad65cafeb08d247d5244fd3f4b409a0db8dc645b
manish2889 <gbontu.3@gmail.com>
2024-02-11 22:50:40 +0530

    Personalised Portfolio

---
+++ src/components/Experience.js
@@ -0,0 +1,36 @@
+const Experience = () => {
+    return (
+        <div className="experience">
+            <h2>Experience</h2>
+
+            <ul>
+                <li>
+                    <i>Concorde International School </i>, 2009-2018
+                    <ul>
+                        <li>Graduated with 85%</li>
+                    </ul>
+                </li>
+                <li>
+                    <i>Mahesh PU College</i> , 2019-2021
+                    <ul>
+                        <li>Graduated with 91%</li>
+                    </ul>
+                </li>
+                <li>
+                    <i>PES College Of Engineering</i> , 2021-2025
+                    <ul>
+                        <li>currently studying with 8 GPA</li>
+                    </ul>
+                </li>
+                <li>
+                    <i>Google Developer Students Club</i> - <strong>Blockchain Developer in technical team</strong>, 2022-present
+                    <ul>
+                        <li>Contributing to the club with various projects</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Experience;
