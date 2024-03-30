313ffd69accda559c9745b679d2f4d6a62dfb32b
Manish <85028609+manish2889@users.noreply.github.com>
2024-03-30 20:23:34 +0530

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -8,6 +8,7 @@ const Social = () => {
             <a href="www.linkedin.com/in/manishgowda" target="_blank" className="button">LinkedIn</a>
             <a href="https://github.com/manish2889" target="_blank" className="button">GitHub</a>
             <a href="https://telegram.dog/callmemanish" target="_blank" className="button">Telegram</a>
+            <a href="https://linktr.ee/manishrgowda" target="_blank" className="button">LinkTree</a>
         </div>
     );
 }
